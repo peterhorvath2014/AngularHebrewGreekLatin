58f401fe155378e06c30a5639327ac66399e07d6
peterhorvath <peter.horvath.2005@gmail.com>
2015-07-02 19:39:13 +0000

    setup fonts

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,25 +8,31 @@
  * Controller of the angularHebrewGreekLatinApp
  */
 angular.module('angularHebrewGreekLatinApp')
-    .controller('MainCtrl', function ($scope, $sanitize, $sce, $log) {
+    .controller('MainCtrl', function($scope, $sanitize, $sce, $log) {
         $scope.tinymceOptions = {
             menubar: "false",
-            toolbar: "newdocument | styleselect | undo redo | bold italic | alignleft aligncenter alignright alignjustify | subscript superscript | fullscreen",
+            toolbar: "newdocument | styleselect | undo redo | bold italic | alignleft aligncenter alignright alignjustify | subscript superscript | removeformat | fullscreen",
             height: "300px",
-            style_formats: [
-                {
-                    title: 'Görög (SPIonic)',
-                    inline: 'span',
-                    styles: {
-                        'font-family': 'SPIonic'
-                    }
+            style_formats: [{
+                title: 'Ελληνική γραμματοσειρά',
+                block: 'p',
+                styles: {
+                    'font-family': 'SPIonic',
                 }
-            ]
+            }, {
+                title: 'סט גופן עברית',
+                block: 'p',
+                styles: {
+                    'font-family': 'Hebrew',
+                    'direction': 'rtl',
+                    'text-align': 'right'
+                }
+            }]
         };
-        $scope.toTrusted = function (htmlCode) {
+        $scope.toTrusted = function(htmlCode) {
             return $sce.trustAsHtml(htmlCode);
         }
-        $scope.emailNotification = function () {
+        $scope.emailNotification = function() {
             var link = "mailto:peter.horvath.2005@gmail.com?subject=Karakter megjelenítés nem működik&body=" + $scope.tinyText;
             window.location.href = link;
         };
